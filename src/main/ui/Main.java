d6114709ce0edfca8c313bae7a3dca1c226fc90a
Sachit Sharma <sachitsharma04@gmail.com>
2024-02-16 11:32:13 -0800

    Made minor changes to adhere to checkstyle

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,5 +1,7 @@
 package ui;
 
 public class Main {
-    public static void main(String[] args) { new StockPortfolioApp();}
+    public static void main(String[] args) {
+        new StockPortfolioApp();
+    }
 }
